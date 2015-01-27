bf5cdb4da8565f17f73377e0921665e10b3f9235
Toby Gosselin <Tobydgosselin@gmail.com>
2015-01-27 17:17:31 -0500

    Added initial interface to log in and authenticate users(not admin and no option to create user yet)

--- blogsrc/blog/views.py
+++ blogsrc/blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+
+
+
 
 def render_to_response(tmpl, data):
     t = loader.get_template(tmpl)
@@ -16,6 +23,29 @@ from blog.models import *
 
 def main(request):
     """Main listing."""
+    if not request.user.is_authenticated():
+        return redirect("accounts/login")
+    posts = Post.objects.all().order_by("createdAt")
+    paginator = Paginator(posts, 2)
+
+    try: page = int(request.GET.get("page", '1'))
+    except ValueError: page = 1
+
+    try:
+        posts = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        posts = paginator.page(paginator.num_pages)
+
+    return render_to_response("list.html", dict(posts=posts, user=request.user))
+
+
+def log_in(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    login(request, user)
+
+def user(request):
     posts = Post.objects.all().order_by("createdAt")
     paginator = Paginator(posts, 2)
 
--- blogsrc/blogsrc/urls.py
+++ blogsrc/blogsrc/urls.py
@@ -6,6 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'blog.views.main', name='home'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^accounts/profile/$', 'blog.views.user', name='home'),
+
     # url(r'^blog/', include('blog.urls')),
     #(r"", "main"),
     url(r'^admin/', include(admin.site.urls)),
