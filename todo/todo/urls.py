2a1a459e1f9e1c8c6352fa61813d2bd1953518fa
Toby Gosselin <Tobydgosselin@gmail.com>
2015-02-03 20:13:36 -0500

    Added  urls for register/login/admin

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^/register/$', 'django.contrib.auth.views.register''blog.views.user', {'template_name': 'register.html'}),
     url(r'^admin/', include(admin.site.urls)),
 )
